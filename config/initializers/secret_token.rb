cc0f6ca84f54e25a270bdd80c3ae96a130bef607
Andrew Pett <astuart.pett@gmail.com>
2014-03-17 22:15:52 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mails::Application.config.secret_key_base = '1a1b5f337d539ba750a45f647e47137c03c44a23cdfc03d18a2a738ddf28ba2095c5710725c19d2b43023b51fe4154a258c4bc3caee061e652c4d06b8b25a52c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mails::Application.config.session_store :cookie_store, key: '_mails_session'
