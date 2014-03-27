f2b3ae4e1c98169c7d8e488abaa5d8df4f9a2a91
Matthew Mortimer <matthew.mortimer1@gmail.com>
2014-03-28 11:28:49 +1300

    in the routes 'resource' is changed to 'resources'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Mails::Application.routes.draw do
   root 'home#index'
-  resource :mails, only: [:index, :show, :new]
-  get '/mail_routes' => 'mail_routes#index', as: "mail_routes_index"
-  resource :mail_routes #index, show, new, create, edit, delete
+  resources :mails
+  resources :mail_routes #index, show, new, create, edit, delete
   # Sets up create/delete route for session controller
-  resource :sessions, only: [:create, :delete]
+  resources :sessions, only: [:create, :delete]
   get "/login" => "sessions#new", as: "login"
 end
