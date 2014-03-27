c84b7b3cec517531b793064713693a9c3bf02fe9
Stephen <stephen@XPS-Ubuntu.(none)>
2014-03-28 12:59:06 +1300

    Added business management controller

---
+++ app/helpers/business_management_helper.rb
@@ -0,0 +1,2 @@
+module BusinessManagementHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Mails::Application.routes.draw do
+  get "business_management/index"
+  get "business_management/figures"
+  get "business_management/logs"
   root 'home#index'
   resources :mails
   resources :mail_routes #index, show, new, create, edit, delete
@@ -6,4 +9,5 @@ Mails::Application.routes.draw do
   # Sets up create/delete route for session controller
   resources :sessions, only: [:create, :delete]
   get "/login" => "sessions#new", as: "login"
+  get "/business_management" => "business_management#index", as: "business_management"
 end
