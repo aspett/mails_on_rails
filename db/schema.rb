a2c917b5c814e592dd9fad1dd6bbabc8b77b6242
Matthew Mortimer <matthew.mortimer1@gmail.com>
2014-03-28 13:02:33 +1300

    Generated place model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140326042113) do
+ActiveRecord::Schema.define(version: 20140327235918) do
+
+  create_table "places", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string "username"
