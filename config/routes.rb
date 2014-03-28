a0763352e47bff593788abb1391bc23176e73de8
Andrew Pett <astuart.pett@gmail.com>
2014-03-28 13:05:08 +1300

    Fixed a merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,7 @@
 Mails::Application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
   get "business_management/index"
   get "business_management/figures"
   get "business_management/logs"
-  get "mail_routes/index"
-  get "mail_routes/show"
-  get "mail_routes/edit"
-  get "mail_routes/new"
-  get "mail_routes/delete"
-  get "home/index"
-=======
->>>>>>> f2b3ae4e1c98169c7d8e488abaa5d8df4f9a2a91
-=======
-  get "business_management/index"
-  get "business_management/figures"
-  get "business_management/logs"
->>>>>>> c84b7b3cec517531b793064713693a9c3bf02fe9
   root 'home#index'
   resources :mails
   resources :mail_routes #index, show, new, create, edit, delete
