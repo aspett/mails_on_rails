6441a930208a334d5b666fe888c25cf99e7e7afe
Andrew Pett <astuart.pett@gmail.com>
2014-04-22 11:37:30 +1200

    Improved user management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,23 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def check_is_manager
+    if !check_logged_in && !current_user.is_manager?
+      redirect_to :root, flash: { error: "Access denied" }
+    end
+  end
+
+  def check_logged_in
+    unless current_user
+      redirect_to :login
+      return true
+    end
+    false
+  end
+
   helper_method :current_user
+  helper_method :check_is_manager
+  helper_method :check_logged_in
+
+
 end
